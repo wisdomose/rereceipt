d40657784da07175d6926b3f7795b86ba336854b
Iyoriobhe Wisdom Osenemendia <35114555+wisdomose@users.noreply.github.com>
2023-02-12 21:05:42 +0100

    feature: disabled state

--- components/button.tsx
+++ components/button.tsx
@@ -1,15 +1,25 @@
 type Props = {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   type?: "button" | "submit";
+  disabled?: boolean;
 };
 
-export default function Button({ type = "submit", ...props }: Props) {
+export default function Button({
+  type = "submit",
+  disabled = false,
+  onClick = () => {},
+  ...props
+}: Props) {
   return (
     <button
       type={type}
-      onClick={props.onClick}
-      className="bg-black/80 text-white rounded-md px-6 py-3 focus:bg-black/70 hover:bg-black/70"
+      onClick={disabled ? undefined : onClick}
+      className={`text-white rounded-md px-6 py-3 ${
+        disabled
+          ? "bg-black/50 cursor-not-allowed"
+          : "bg-black/80 focus:bg-black/70 hover:bg-black/70 "
+      }`}
     >
       {props.label}
     </button>
