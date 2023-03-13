cb87f5513028b4a00fcd35a0a9c7ea481aee22fd
Iyoriobhe Wisdom Osenemendia <35114555+wisdomose@users.noreply.github.com>
2023-03-13 22:37:42 +0100

    feature: error boundary design

--- components/layout/ErrorBoundary.tsx
+++ components/layout/ErrorBoundary.tsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import img from "../../src/img/assets/error.png";
+import { log } from "next-axiom";
 
 class ErrorBoundary extends React.Component {
   constructor(props: any) {
@@ -14,21 +18,31 @@ class ErrorBoundary extends React.Component {
   }
   componentDidCatch(error: any, errorInfo: any) {
     // You can use your own error logging service here
-    console.log({ error, errorInfo });
+    log.warn("Error boundary", { error, errorInfo });
   }
   render() {
     // Check if the error is thrown
     if ((this.state as any).hasError) {
       // You can render any custom fallback UI
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button
-            type="button"
-            onClick={() => this.setState({ hasError: false })}
-          >
-            Try again?
-          </button>
+        <div className="h-full grid place-items-center text-[#4F4F4F] p-6">
+          <div className="h-fit w-full">
+            <div className="relative w-full sm:w-1/2 md:w-1/3 mx-auto aspect-square">
+              <Image src={img} alt="" fill />
+            </div>
+
+            <p className="text-sm max-w-[402] text-center">
+              <span className="font-semibold text-xl mb-1 inline-block">
+                we encountered an error.
+              </span>
+              <br /> our development team has been contacted and it will be
+              fixed soonest
+              <br />
+              <Link href="/" className="font-semibold underline">
+                homepage
+              </Link>
+            </p>
+          </div>
         </div>
       );
     }
