9ac9312f9e437e958ba4a81ee50b858670a80f02
Iyoriobhe Wisdom Osenemendia <35114555+wisdomose@users.noreply.github.com>
2023-03-27 03:08:55 +0100

    chore: build warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserContextProvider>
       <Head>
+        <title>Rereceipt</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
